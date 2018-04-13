718a7e4fe3e47a235eda59ae02a0cdcf129b7b6c
andjuni <sifacai@msn.com>
2018-04-13 17:15:01 +0800

    2018-04-13

--- reptile.js
+++ reptile.js
@@ -1,2 +1,21 @@
+var https = require("https");
 var http = require("http");
 var qs = require("querystring");
+//var $ = require("jquery");
+
+
+
+var cc =sendURL('https://www.baidu.com');
+
+function sendURL(url){
+	var resdata="";
+	https.get(url,function(req,res){
+		req.on("data",function(data){
+			resdata+=data;
+		});
+		req.on("end",function(){
+			console.info(resdata);
+		});
+	});
+}
+
